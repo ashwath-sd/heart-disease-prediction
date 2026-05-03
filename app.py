52652c766e8d332e516773bc0d136c7f29b727c8
Ashwath S D <ashwathash412@gmail.com>
2026-05-03 19:45:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ st.title("Heart Disease Prediction")
 st.write("App is running...")
 
 try:
-    model_path = os.path.join(os.getcwd(), "gboost_model.pkl")
-    model = pickle.load(open(gboost_model_path, "rb"))
+ model_path = os.path.join(os.getcwd(), "gb_model.pkl")
+model = pickle.load(open(model_path, "rb"))
 
     st.success("Model loaded successfully")
 
